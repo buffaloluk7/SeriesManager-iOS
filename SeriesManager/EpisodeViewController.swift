09c8326751121f497f0036c42d95a32e35209c97
Max Berg <maxberg1c@icloud.com>
2015-01-14 09:49:25 +0100

    update UI

--- SeriesManager/EpisodeViewController.swift
+++ SeriesManager/EpisodeViewController.swift
@@ -15,13 +15,20 @@ class EpisodeViewController: UIViewController, TheTVDBApiDelegate {
     
     @IBOutlet weak var episodeImage: UIImageView!
     @IBOutlet weak var episodeName: UILabel!
+    @IBOutlet weak var episodeOverview: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        episodeOverview.editable = true;
+        episodeOverview.font = UIFont(name: "Helvetica", size: 17)
+        episodeOverview.editable = false;
 
         self.theTVDBApi.apiDelegate = self
         self.title = episode.name
         self.episodeName.text = episode.name
+        self.episodeOverview.text = episode.overview;
         
         // Load the episode image.
         if episode.imagePath != nil {
